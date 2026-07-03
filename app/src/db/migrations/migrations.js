ff8bfb1e79b03c2892a9601a951d302cea89bb76
riolly <alberto.cahyantara@gmail.com>
2026-07-03 15:14:03 +0900

    bump

--- app/src/db/migrations/migrations.js
+++ app/src/db/migrations/migrations.js
@@ -1,12 +1,8 @@
-// This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
-
-import journal from './meta/_journal.json';
-import m0000 from './0000_large_grey_gargoyle.sql';
+import m0000 from './20260629050607_large_grey_gargoyle/migration.sql';
 
   export default {
-    journal,
     migrations: {
-      m0000
-    }
+      "20260629050607_large_grey_gargoyle": m0000
+}
   }
   
